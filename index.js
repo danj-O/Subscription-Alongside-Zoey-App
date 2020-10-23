18bf0eec1546b3527cc2ebfac7c9127c1d443af0
danj-O <danjowanjo@gmail.com>
2020-10-23 17:58:13 -0400

    finds customers who have made multiple orders and puts them into an array to eventually be compared

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.listen(PORT, function(){
   console.log("Server is running on port 3000")
 })
 
+
+// -----------------START----------------------------------------------------------------- 
+
 const baseUrl = 'https://2ieb7j62xark0rjf.mojostratus.io'
 
 let date_ob = new Date();
@@ -30,50 +33,79 @@ let date = ("0" + date_ob.getDate()).slice(-2);
 
 const fullDate = year + "-" + month + "-" + date
 
-getAllAll()
+getAll()
 
-async function getAllAll(){
-  // allCustomers = await getDataWithAuth('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/customers/29')
-  // allCustomers = await getDataWithAuth('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/customers/search?searchCriteria[sortOrders][0][field]=email&searchCriteria[sortOrders][0][direction]=asc')
-  // const allOrders = await getDataWithAuth('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/orders?searchCriteria=all')
-  // allProducts = await getDataWithAuth('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/products?searchCriteria=all')
+async function getAll(){
+  const orders = await getData('/rest/V1/orders?searchCriteria=all')
+  await orders.sort(compareEmail)
+  const multiPurchaseCustEmails = getMultiplePurchaseCustomerEmails(orders)
+  console.log(multiPurchaseCustEmails)
+  // build an object for each customer containing the 
 
-  // await console.log("PRODUCTS", allProducts.data.items[0].extension_attributes.website_ids)
-  const allProductIds = await getAllProductIds()
-  console.log("all product ids", allProductIds)
-  // const allCustomerNames = await getAllCustomerNames()
-  // console.log("allCustomerNames", allCustomerNames)
-  
-  // await console.log("ORDERS",allOrders.data.items.length)
-  // await console.log("CUSTOMERS", typeof( allCustomers.data))
 
-  // allProducts = await getAllPages(1, `https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/products/?SearchCriteria[pageSize]=10`)
-  // todaysOrders = await getTodaysOrders(`n`, `https://ts967672-container.zoeysite.com/api/rest/orders?`)
+  // const allCustomerNames = await getData('/rest/V1/customers/search?searchCriteria[sortOrders][0][field]=email&searchCriteria[sortOrders][0][direction]=asc', 'firstname')
 }
+
+async function getData(suffix, param){
+  let response = await getDataWithAuth(`${baseUrl}${suffix}`)
+  // console.log(response.data.items)
+  const result = []
+  response.data.items.map(item => {
+    result.push({
+      name : item.billing_address.firstname,
+      email : item.customer_email,
+      date : item.created_at,
+      items : item.items,
+      orderNumber : item.increment_id,
+      total : item.base_grand_total
+    })
+  })
+  return result
+}
+
+//FUNCTION THAT COMPARES ALL ORDERS MADE BY ONE PERSON FOR SIMILARITIES IN PRODUCTS PURCHASED
+
+//IF THERE ARE SIMILARITIES, CHECK THE FREQUENCY OF THESE ORDERS (HOW FREQUENT IS ENOUGH???)
+
+//IF THERE ARE SIMILARITIES AND FREQUENCY, NOTIFY ADMIN AND CUSTOMER
+
+function getMultiplePurchaseCustomerEmails(orders){
+  let prevOrder = {}
+  const emails = []
+  orders.map(order => {
+    if (order.email == prevOrder.email){
+      // add a param to order obj?
+      // order.purchaseCount++
+      emails.push(order.email)
+    }
+    prevOrder = order
+  })
+  //uniqueemails are all of the emails of customers who have made multiple purchases
+  const uniqueEmails = [...new Set(emails)]
+  return uniqueEmails
+}
+
+function compareEmail(a, b) {
+  const bandA = a.email;
+  const bandB = b.email;
+  let comparison = 0;
+  if (bandA > bandB) {
+    comparison = 1;
+  } else if (bandA < bandB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+
+
+
 
 async function getDataWithAuth(url){
   const request = { url: url, method: 'GET', };
   const authHeader = Oauth1Helper.getAuthHeaderForRequest(request);
   return await axios.get( request.url, { headers: authHeader });
 }
-
-async function getAllProductIds(){
-  const allProducts = await getDataWithAuth(`${baseUrl}/rest/V1/products?searchCriteria=all`)
-  const result = []
-  allProducts.data.items.map(product => result.push(product.id))
-  return result
-}
-async function getAllCustomerNames(){
-  const allCustomers = await getDataWithAuth(`${baseUrl}/rest/V1/customers/search?searchCriteria[sortOrders][0][field]=email&searchCriteria[sortOrders][0][direction]=asc`)
-  const result = []
-  allCustomers.data.items.map(product => result.push(product.firstname))
-  return result
-}
-
-
-
-
-
 
 
 
