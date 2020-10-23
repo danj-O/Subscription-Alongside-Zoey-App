000c148e5359106b9a45008b3bb993f608eac93c
danj-O <danjowanjo@gmail.com>
2020-10-23 13:56:15 -0400

    just logging productids to test on heroku

--- index.js
+++ index.js
@@ -20,9 +20,8 @@ app.listen(3000, function(){
   console.log("Server is running on port 3000")
 })
 
-let allCustomers = {};
-let allProducts = {};
-let todaysOrders = {};
+const baseUrl = 'https://2ieb7j62xark0rjf.mojostratus.io'
+
 let date_ob = new Date();
 let year = date_ob.getFullYear();
 let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
@@ -33,49 +32,77 @@ const fullDate = year + "-" + month + "-" + date
 getAllAll()
 
 async function getAllAll(){
-  // allCustomers = await getDataWithAuth2('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/customers/1')
-  const allOrders = await getDataWithAuth2('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/orders?searchCriteria=all')
-  // allProducts = await getDataWithAuth2('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/products?searchCriteria=all')
+  // allCustomers = await getDataWithAuth('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/customers/29')
+  // allCustomers = await getDataWithAuth('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/customers/search?searchCriteria[sortOrders][0][field]=email&searchCriteria[sortOrders][0][direction]=asc')
+  // const allOrders = await getDataWithAuth('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/orders?searchCriteria=all')
+  // allProducts = await getDataWithAuth('https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/products?searchCriteria=all')
 
-  // await console.log(allProducts.data)
-  await console.log(allOrders.data.items.length)
+  // await console.log("PRODUCTS", allProducts.data.items[0].extension_attributes.website_ids)
+  const allProductIds = await getAllProductIds()
+  console.log("all product ids", allProductIds)
+  // const allCustomerNames = await getAllCustomerNames()
+  // console.log("allCustomerNames", allCustomerNames)
+  
+  // await console.log("ORDERS",allOrders.data.items.length)
+  // await console.log("CUSTOMERS", typeof( allCustomers.data))
 
   // allProducts = await getAllPages(1, `https://2ieb7j62xark0rjf.mojostratus.io/rest/V1/products/?SearchCriteria[pageSize]=10`)
   // todaysOrders = await getTodaysOrders(`n`, `https://ts967672-container.zoeysite.com/api/rest/orders?`)
 }
 
-
-async function getDataWithAuth2(url){
+async function getDataWithAuth(url){
   const request = { url: url, method: 'GET', };
   const authHeader = Oauth1Helper.getAuthHeaderForRequest(request);
   return await axios.get( request.url, { headers: authHeader });
 }
 
-
-
-async function getAllPages(n, urlPartial) {
-  let prevPageData = {}
-  const allData = {}
-  for (i=1; ; i++){
-    const urlCat = `${urlPartial}page=${i}`
-    const data = await getDataWithAuth(urlCat)
-    if (JSON.stringify(data) === JSON.stringify(prevPageData)){
-      break
-    }
-    prevPageData = {}
-    for (key in await data){
-      prevPageData[key] = data[key]
-      allData[key] = data[key]
-    }
-  }
-  await console.log('GET ALL DATA COMPLETE')
-  return await allData
+async function getAllProductIds(){
+  const allProducts = await getDataWithAuth(`${baseUrl}/rest/V1/products?searchCriteria=all`)
+  const result = []
+  allProducts.data.items.map(product => result.push(product.id))
+  return result
+}
+async function getAllCustomerNames(){
+  const allCustomers = await getDataWithAuth(`${baseUrl}/rest/V1/customers/search?searchCriteria[sortOrders][0][field]=email&searchCriteria[sortOrders][0][direction]=asc`)
+  const result = []
+  allCustomers.data.items.map(product => result.push(product.firstname))
+  return result
 }
 
-async function getTodaysOrders(n, urlPartial){
-  // const allData = {}
-  const urlCat = `${urlPartial}filter[1][attribute]=created_at&filter[1][gte]=${fullDate}%2000:00:00&filter[2][attribute]=created_at&filter[2][lte]=${fullDate}%2023:59:59&`
-  //returns all the pages for the orders
-  const data = await getAllPages(n, urlCat)
-  return await data
-}
+
+
+
+
+
+
+
+
+
+
+
+// async function getAllPages(n, urlPartial) {
+//   let prevPageData = {}
+//   const allData = {}
+//   for (i=1; ; i++){
+//     const urlCat = `${urlPartial}page=${i}`
+//     const data = await getDataWithAuth(urlCat)
+//     if (JSON.stringify(data) === JSON.stringify(prevPageData)){
+//       break
+//     }
+//     prevPageData = {}
+//     for (key in await data){
+//       prevPageData[key] = data[key]
+//       allData[key] = data[key]
+//     }
+//   }
+//   await console.log('GET ALL DATA COMPLETE')
+//   return await allData
+// }
+
+// async function getTodaysOrders(n, urlPartial){
+//   // const allData = {}
+//   const urlCat = `${urlPartial}filter[1][attribute]=created_at&filter[1][gte]=${fullDate}%2000:00:00&filter[2][attribute]=created_at&filter[2][lte]=${fullDate}%2023:59:59&`
+//   //returns all the pages for the orders
+//   const data = await getAllPages(n, urlCat)
+//   return await data
+// }
